97c0ac777ac38d7b0f6693797d8fbe609ce05a71
“ABSteingit config --global user.email “abs19stein@gmail.com <andrewstein@Andrews-MacBook-Pro-2.local>
2017-08-29 19:50:23 -0500

    contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,4 +3,9 @@ class ContactsController < ApplicationController
     @contact = Contact.first
     render 'one_contact_page.html.erb'
   end
+
+  def all_contacts_action
+    @contacts = Contact.all 
+    render 'all_contacts_page.html.erb'
+  end
 end
