d327bf286ee28b30842240cd4a1836f0a3d8c4d5
“ABSteingit config --global user.email “abs19stein@gmail.com <andrewstein@Andrews-MacBook-Pro-2.local>
2017-09-07 21:33:12 -0500

    changes

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,11 +1,45 @@
 class ContactsController < ApplicationController
-  def one_contact_action
-    @contact = Contact.first
-    render 'one_contact_page.html.erb'
+  def index
+    @contacts = Contact.all 
   end
 
-  def all_contacts_action
-    @contacts = Contact.all 
-    render 'all_contacts_page.html.erb'
+  def new
+    
+  end
+  
+  def create
+    contact = Contact.new( 
+                          first_name: params[:first_name],
+                          last_name: params[:last_name],
+                          middle_name: params[:middle_name],
+                          email: params[:email],
+                          phone_number: params[:phone_number]
+                          )
+
+    contact.save
+  end
+
+  def show
+    @contact = Contact.find(params[:id]) 
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+
+    assign_attributes( 
+                      first_name: params[:first_name],
+                      last_name: params[:last_name],
+                      middle_name: params[:middle_name],
+                      email: params[:email],
+                      phone_number: params[:phone_number]
+                      )
+  end
+
+  def delete
+    @contact = Contact.find(params[:id])
   end
 end
